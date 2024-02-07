34dfe046319813766049e1830762b54ff2f3cbe8
unexplainednerd <157606755+unexplainednerd@users.noreply.github.com>
2024-02-07 21:21:41 +0400

    goal oriented academy homework
    goal oriented academy homework

---
+++ lesson 1.py
@@ -0,0 +1,17 @@
+print("(who i became friends with in goal oriented academy).")
+print("vano motiashvili")
+print("me and vano motiashvili became friends online")
+print("vano motiashvili helped me learn about goal oriented academy")
+print("without him i wouldnt be able to understand anything...")
+
+
+
+print("(what i like about goal oriented academy and why i like it)")
+print('goal oriented academy is a very good learning academy')
+print("cuz goal oriented academy doesent just teach u programming it teaches you how to be a chad and live a good life")
+print("goal oriented academy has a lot of good members and u can make alot of friends")
+print("there are amazing teachers and leaders")
+
+
+
+print("goal oriented academy is the best academy in the world!!!")
